8412f424f123ef0c474e2711ab8530a8ad009536
Ariel Weingarten <aweingarten@symphonycommerce.com>
2015-10-10 14:59:40 -0700

    Headlines link to audio.
    - Article interface for results from server
    - Update HeadlineProps interface

--- static/app/scripts/kno.tsx
+++ static/app/scripts/kno.tsx
@@ -2,27 +2,37 @@
 /// <reference path="../typings/jquery/jquery.d.ts" />
 import React = __React;
 
-
-// React Views
-interface HeadlineProps extends React.Props<any> {
+interface Article {
+  title: string;
+  id: string;
+  entities: string[];
+  image: any;
+  url: string;
   text: string;
 }
 
+// React Views
+interface HeadlineProps extends React.Props<any> {
+  title: string;
+  id: string;
+}
+
 interface HeadlinesProps extends React.Props<any> {
-  headlines: string[];
+  articles: Article[];
 }
 
 class Headline extends React.Component<HeadlineProps, {}> {
   render() {
-    return <li>{this.props.text}</li>;
+    let link = 'http://kno.ccl.io:6001/static/wav/' + this.props.id + '.wav';
+    return <li><a href={link}> {this.props.title}</a></li>;
   }
 }
 
 class Headlines extends React.Component<HeadlinesProps, {}> {
   render() {
-    var headlines = this.props.headlines;
-    var headlineElements = headlines.map(function(headline) {
-      return (<Headline text={headline} />);
+    var articles = this.props.articles;
+    var headlineElements = articles.map(function(article) {
+      return (<Headline title={article.title} id={article.id} />);
     }, this);
     
     return (
@@ -44,13 +54,15 @@ $(() => {
   
     $.get(apiEndpoint, { term: 'Apple' }, (rawArticles:string) => {
       // console.log(articles);
-      let articles:any[] = JSON.parse(rawArticles);
-      headlines = $.map(articles, function(article) {
-        return article.title;
-      });
+      let articles:Article[] = JSON.parse(rawArticles);
+      // headlines = $.map(articles, function(article) {
+      //   return {
+      //     title: article.title,
+      //     id: article.id
+      // });
       
-      console.log(headlines);
-      content = <Headlines headlines={headlines} />;
+      console.log(articles);
+      content = <Headlines articles={articles} />;
       React.render(content,target);
     });
     
